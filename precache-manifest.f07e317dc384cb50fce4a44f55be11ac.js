c0c05c3d1ebfeda5434e483291df8a1c46dbb783
fernandodcastanon <fernandodaniel777castanon@gmail.com>
2019-10-08 21:18:42 -0700

    Updates

---
+++ precache-manifest.f07e317dc384cb50fce4a44f55be11ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fbd8c5d73572fbefdbbc060727718d",
+    "url": "/harder/index.html"
+  },
+  {
+    "revision": "a05d87dd6f5369806efe",
+    "url": "/harder/static/css/main.ecd6353c.chunk.css"
+  },
+  {
+    "revision": "00be09649216f1edfba2",
+    "url": "/harder/static/js/2.d9b656de.chunk.js"
+  },
+  {
+    "revision": "a05d87dd6f5369806efe",
+    "url": "/harder/static/js/main.658b21c1.chunk.js"
+  },
+  {
+    "revision": "9bad7e5fa1f97982b371",
+    "url": "/harder/static/js/runtime-main.8bceaaf9.js"
+  }
+]);
